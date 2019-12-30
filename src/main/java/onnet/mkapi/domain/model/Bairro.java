ddcfdd54dfbec3dfbe9eb0ee336604f249ad2ef8
Vinícius Gomes <desenvolvimento02.onnetmais@gmail.com>
2019-12-30 16:53:11 -0300

    Implementacao Lombok Bairro

--- src/main/java/onnet/mkapi/domain/model/Bairro.java
+++ src/main/java/onnet/mkapi/domain/model/Bairro.java
@@ -5,7 +5,11 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 @Entity
+@Data
 @Table(name = "mk_bairros")
 public class Bairro {
 
@@ -14,49 +18,6 @@ public class Bairro {
 	private long id;
 
 	@Column(name = "bairro")
-	private String bairro;
-
-	public long getId() {
-		return id;
-	}
-
-	public void setId(long id) {
-		this.id = id;
-	}
-
-	public String getBairro() {
-		return bairro;
-	}
-
-	public void setBairro(String bairro) {
-		this.bairro = bairro;
-	}
-
-	@Override
-	public String toString() {
-		return "Bairro [id=" + id + ", bairro=" + bairro + "]";
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + (int) (id ^ (id >>> 32));
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Bairro other = (Bairro) obj;
-		if (id != other.id)
-			return false;
-		return true;
-	}
+	@EqualsAndHashCode.Exclude private String bairro;
 
 }
